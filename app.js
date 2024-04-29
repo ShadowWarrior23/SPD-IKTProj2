d1aae757cce64865e6a29f07b2a8de3084137da6
Shadow <gamernagylolhih@gmail.com>
2024-04-29 18:04:17 +0200

    Kép optim

---
+++ app.js
@@ -0,0 +1,6 @@
+const popupButton = document.querySelector("#form");
+const popupWindow = document.querySelector("form");
+
+popupButton.addEventListener("click", function() {
+    popupWindow.style.display = "block";
+});
