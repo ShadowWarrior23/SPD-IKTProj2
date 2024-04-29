c227308771a9c0cb563748311174aac6551da483
Shadow <gamernagylolhih@gmail.com>
2024-04-29 18:17:08 +0200

    !

--- app.js
+++
@@ -1,6 +0,0 @@
-const popupButton = document.querySelector("#form");
-const popupWindow = document.querySelector("form");
-
-popupButton.addEventListener("click", function() {
-    popupWindow.style.display = "block";
-});
