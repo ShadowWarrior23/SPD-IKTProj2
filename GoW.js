902f7d773afd11bbfc351f16b29daebd73eaeed5
Shadow <gamernagylolhih@gmail.com>
2024-04-30 17:17:44 +0200

    Valami1

--- GoW.js
+++ GoW.js
@@ -1,3 +1,4 @@
+//Rendelőfelület popup
 const popupButton = document.querySelector("#form");
 const popupWindow = document.querySelector("section");
 
@@ -10,8 +11,15 @@ closeButton.addEventListener("click", () => {
     popupWindow.style.display = "none";
 })
 
+//Játék popup
+const popupButton1 = document.querySelector("#gameSelect");
+const popupWindow1 = document.querySelector(".game");
 
+popupButton1.addEventListener("click", function() {
+    popupWindow1.style.display = "block";
+});
 
-
-/*Popup*/
-
+const closeButton1 = document.querySelector("#close-button");
+closeButton1.addEventListener("click", () => {
+    popupWindow1.style.display = "none";
+})
