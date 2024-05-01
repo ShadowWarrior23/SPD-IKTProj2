31508e642bba34d4353e7cd433d09c84a5c95236
bottyan.martin <bottyan.martin>
2024-05-01 17:01:06 +0200

    termékek

--- GoW.js
+++ GoW.js
@@ -10,16 +10,3 @@ const closeButton = document.querySelector("#close-button");
 closeButton.addEventListener("click", () => {
     popupWindow.style.display = "none";
 })
-
-//Játék popup
-const popupButton1 = document.querySelector("#gameSelect");
-const popupWindow1 = document.querySelector(".game");
-
-popupButton1.addEventListener("click", function() {
-    popupWindow1.style.display = "block";
-});
-
-const closeButton1 = document.querySelector("#close-button");
-closeButton1.addEventListener("click", () => {
-    popupWindow1.style.display = "none";
-})
